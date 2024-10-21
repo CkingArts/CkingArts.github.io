7142cb213a1990583e61cd1235f1112a4f9a8d53
CkingArts <Chloe.King@sjsu.edu>
2024-10-21 14:33:17 -0700

    replaced files

--- the_sketch/the_sketch.js
+++ the_sketch/the_sketch.js
@@ -1,8 +1,18 @@
 function setup() {
-createCanvas(720, 680);
+createCanvas(307, 400);
 }
 function draw() {
 background(204);
-ellipse(350, 200, 200, 250);
-triangle(350, 230, 400, 230, 300, 160);
+strokeWeight(1);
+fill(240,198,70);
+ellipse(153,150,100,123);
+triangle(153,164,146,175,159,175);
+fill(243,255,231);
+ellipse(135,149,16,8);
+ellipse(170,149,16,8);
+fill(240,100,100)
+strokeWeight(0);
+quad(145,188,153,184,160,188,153,193)
+strokeWeight(1);
+line(144,188,161,188);
 }
